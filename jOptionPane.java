84c034d6b92f4219f96b4a88db69832340b1dbf2
ARNIMAGOEL <82924354+ARNIMAGOEL@users.noreply.github.com>
2023-08-04 23:25:36 +0530

    my first commit

---
+++ jOptionPane.java
@@ -0,0 +1,18 @@
+import javax.swing.JRootPane;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Arnima
+ */
+class jOptionPane {
+
+    static void showMessageDialog(JRootPane rootPane, String all_fields_should_be_filled_) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
